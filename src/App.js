caba724613196118ae9e5ba6c68e6d6d716858c7
Dale Bendixen <dalebendixen@gmail.com>
2022-08-15 19:43:50 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count)
+    if (count <= 0){
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+   setText(data.slice(0, amount));
+   
   }
 
   return (
@@ -22,8 +30,9 @@ function App() {
         <button type='submit' className='btn'>generate</button>
       </form>
       <article>
-        <p className='lorem-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, possimus.</p>
-        <p className='lorem-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique, possimus.</p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
       </article>
     </section>
     )
